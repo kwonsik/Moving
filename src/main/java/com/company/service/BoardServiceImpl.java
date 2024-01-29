4b030b22bc7cb9dea7fe104d4badecb55df035da
Ilhawn Yu <u1ghks@gmail.com>
2024-01-29 17:08:13 +0900

    fix:boardServiceImple 오류수정

--- src/main/java/com/company/service/BoardServiceImpl.java
+++ src/main/java/com/company/service/BoardServiceImpl.java
@@ -12,22 +12,22 @@ import com.company.dto.BoardDto;
 
 @Service
 public class BoardServiceImpl implements BoardService {
-	@Autowired BoardDao dao;
-	
-	@Override public int b_insert(BoardDto dto) {
-		try { dto.setB_ip(InetAddress.getLocalHost().getHostAddress());
-		} catch (UnknownHostException e) { e.printStackTrace();
-		} return dao.b_insert(dto);
-	}
+   @Autowired BoardDao dao;
+   
+   @Override public int b_insert(BoardDto dto) {
+      try { dto.setB_ip(InetAddress.getLocalHost().getHostAddress());
+      } catch (UnknownHostException e) { e.printStackTrace();
+      } return dao.b_insert(dto);
+   }
 
-	@Override public int b_update(BoardDto dto) { return dao.b_update(dto); }
-	@Override public int b_delete(BoardDto dto) { return dao.b_delete(dto); }
+   @Override public int b_update(BoardDto dto) { return dao.b_update(dto); }
+   @Override public int b_delete(BoardDto dto) { return dao.b_delete(dto); }
 
-	@Override public BoardDto b_readDetail(int board_no) {
-		dao.b_update_hit(board_no);
-		return dao.b_read(board_no);
-	}
+   @Override public BoardDto b_readDetail(int board_no) {
+      dao.b_update_hit(board_no);
+      return dao.b_read(board_no);
+   }
 
-	@Override public BoardDto b_updateView(int board_no) { return dao.b_read(board_no); }
-	@Override public List<BoardDto> b_readAll() { return dao.b_readAll(); }
+   @Override public BoardDto b_updateView(int board_no) { return dao.b_read(board_no); }
+   @Override public List<BoardDto> b_readAll() { return dao.b_readAll(); }
 }
